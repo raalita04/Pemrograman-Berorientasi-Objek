02e86ff7735b0fe63a250e5d6e3df8152bce1081
raalita04 <114082628+raalita04@users.noreply.github.com>
2022-10-17 20:07:41 +0700

    Add files via upload

---
+++ Sesi1/ProgramJava.java
@@ -0,0 +1,13 @@
+public class ProgramJava {
+    public static void main(String[]args){
+        //No 1
+        
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+        
+ 
+    }
+    
+    
+}
---
+++ Sesi1/ProgramJavaGUI.java
@@ -0,0 +1,12 @@
+
+import javax.swing.*;
+
+public class ProgramJavaGUI {
+    public static void main(String[]args){
+        String Inputkalimat = JOptionPane.showInputDialog("Anda Sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar " + Inputkalimat + " sangat menyenangkan ");
+        
+
+    }
+    
+}
