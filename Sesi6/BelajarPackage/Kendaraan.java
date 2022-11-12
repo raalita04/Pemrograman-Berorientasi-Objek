e4098595dd648d777dbf267aa2a3489999aca55a
raalita04 <114082628+raalita04@users.noreply.github.com>
2022-11-12 19:41:42 +0700

    Add files via upload

---
+++ Sesi6/BelajarPackage/Kendaraan.java
@@ -0,0 +1,9 @@
+package com.alat.transportasi;
+import com.alat.transportasi.beroda.KendaraanBeroda;
+
+public class Kendaraan {
+    public static void main(String[] args){
+        System.out.println("Belajar package dan interface");
+    }
+    
+}
